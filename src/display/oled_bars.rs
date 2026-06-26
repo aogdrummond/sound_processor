2552e109c2da92b0c2ed4cadfed1211aa802f8b4
Alexandre Otávio Guedes Drummond <aod@ngrt.org>
2026-06-26 18:40:25 -0300

    Commiting

--- src/display/oled_bars.rs
+++ src/display/oled_bars.rs
@@ -120,9 +120,12 @@ impl OledBars {
 
             let normalized = (value / 80.0).clamp(0.0, 1.0).sqrt();
 
-            let bar_height = (normalized * height as f32) as i32;
-
+            let bar_height =
+                (normalized * height as f32)
+                    .round()
+                    .clamp(0.0, height as f32) as i32;
             let x = (i * bar_width) as i32;
+            
 
             let y = height as i32 - bar_height;
 
