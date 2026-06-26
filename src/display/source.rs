dcea521e17dbd315bb8006ec1e5c564785094e48
Alexandre Otávio Guedes Drummond <aod@ngrt.org>
2026-06-25 21:41:51 -0300

    Compiling

--- src/display/source.rs
+++ src/display/source.rs
@@ -1,5 +1,10 @@
 use std::sync::mpsc;
 
+pub struct AudioFrame {
+    pub timestamp: Instant,
+    pub samples: Vec<f32>,
+}
+
 pub trait DisplaySource {
     fn display_results(&mut self, rx_bands: mpsc::Receiver<AudioFrame>);
 }
